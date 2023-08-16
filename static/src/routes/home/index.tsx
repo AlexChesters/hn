cdf18b288498992d8df4598755ff8491ceef5cec
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-08-16 15:01:42 +0100

    switch

--- static/src/routes/home/index.tsx
+++ static/src/routes/home/index.tsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react'
+// import { useUpdateEffect } from 'react-use'
 import List from '@mui/material/List'
 import ListItem from '@mui/material/ListItem'
 import ListItemButton from '@mui/material/ListItemButton'
 import ListItemText from '@mui/material/ListItemText'
 import Divider from '@mui/material/Divider'
+import Button from '@mui/material/Button'
 
 import { Post } from '../../types'
 
@@ -22,27 +24,35 @@ const Home = () => {
 
   useEffect(() => {
     fetchData(storiesIndex)
-  }, [])
+  }, [storiesIndex])
+
+  const seeMore = () => {
+    console.log('see more')
+    setStoriesIndex(storiesIndex + 1)
+  }
 
   if (!stories) return null
 
   return (
-    <List>
-      {
-        stories.map((story, index) => {
-          return (
-            <div key={index}>
-              <ListItem>
-                <ListItemButton>
-                  <ListItemText>{story.title}</ListItemText>
-                </ListItemButton>
-              </ListItem>
-              <Divider />
-            </div>
-          )
-        })
-      }
-    </List>
+    <>
+      <List>
+        {
+          stories.map((story, index) => {
+            return (
+              <div key={index}>
+                <ListItem>
+                  <ListItemButton>
+                    <ListItemText>{story.title}</ListItemText>
+                  </ListItemButton>
+                </ListItem>
+                <Divider />
+              </div>
+            )
+          })
+        }
+      </List>
+      <Button onClick={() => seeMore()}>See more</Button>
+    </>
   )
 }
 
