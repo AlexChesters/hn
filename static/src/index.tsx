1e7ba6bf8f71b7bf76ab7cc403cfe13254770011
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-08-16 14:55:25 +0100

    display data

--- static/src/index.tsx
+++ static/src/index.tsx
@@ -3,5 +3,10 @@ import { createRoot } from 'react-dom/client'
 
 import Router from './router'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 const root = createRoot(document.getElementById('app'))
 root.render(<Router />)
--- static/src/routes/home/index.tsx
+++ static/src/routes/home/index.tsx
@@ -1,18 +1,48 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemButton from '@mui/material/ListItemButton'
+import ListItemText from '@mui/material/ListItemText'
+import Divider from '@mui/material/Divider'
+
+import { Post } from '../../types'
 
 import { topStories } from '../../networking/hn'
 
 const Home = () => {
-  async function fetchData () {
-    console.log(await topStories())
+  const [stories, setStories] = useState<Post[]>(null)
+  const [storiesIndex, setStoriesIndex] = useState<number>(0)
+
+  async function fetchData (index: number) {
+    const topStoriesData = await topStories(index)
+
+    setStoriesIndex(topStoriesData.index)
+    setStories(topStoriesData.posts)
   }
 
   useEffect(() => {
-    fetchData()
+    fetchData(storiesIndex)
   }, [])
 
+  if (!stories) return null
+
   return (
-    <h1>Hello, world!</h1>
+    <List>
+      {
+        stories.map((story, index) => {
+          return (
+            <div key={index}>
+              <ListItem>
+                <ListItemButton>
+                  <ListItemText>{story.title}</ListItemText>
+                </ListItemButton>
+              </ListItem>
+              <Divider />
+            </div>
+          )
+        })
+      }
+    </List>
   )
 }
 
