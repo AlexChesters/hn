74156842c2fff29476e9aaf3941e27e481d19bf2
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2023-08-18 14:07:33 +0100

    only include stories

--- static/src/networking/hn.ts
+++ static/src/networking/hn.ts
@@ -19,21 +19,24 @@ export const topStories = async (index = 0): Promise<TopStoriesResponse> => {
 
   const chunk = chunkArray(postIdsData, 10)[index]
 
-  const promises = chunk.map(async (id) => {
-    const postRes = await window.fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
-    const postData = await postRes.json()
+  const promises = chunk
+    .map(async (id) => {
+      const postRes = await window.fetch(`https://hacker-news.firebaseio.com/v0/item/${id}.json`)
+      const postData = await postRes.json()
 
-    return {
-      id,
-      title: postData.title,
-      link: new URL(postData.url)
-    }
-  })
+      if (postData.type !== 'story') return null
+
+      return {
+        id,
+        title: postData.title,
+        link: new URL(postData.url)
+      }
+    })
 
   const posts = await Promise.all(promises)
 
   return {
-    posts,
+    posts: posts.filter(Boolean),
     index
   }
 }
